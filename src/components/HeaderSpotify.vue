c38898a1782921a0358b27ba2bff9b63b2f10af7
LaSe28 <mattia.las28@gmail.com>
2022-03-24 15:45:40 +0100

    start

---
+++ src/components/HeaderSpotify.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSpotify'
+}
+</script>
+
+<style scoped lang="scss">
+  header{
+    height: 5rem;
+    background-color: #303A45;
+  }
+</style>
---
+++ src/components/MainSpotify.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SongsSpotify/>
+  </div>
+</template>
+
+<script>
+import SongsSpotify from './SongsSpotify.vue'
+
+export default {
+  name: 'MainSpotify',
+  components: {
+    SongsSpotify
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
